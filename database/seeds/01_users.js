fe5072975ccb8570418f5fb12204a54d1645c59e
CMunkyDev <matt@jcmueller.com>
2018-03-24 06:16:04 -0700

    add seeds

--- database/seeds/01_users.js
+++ database/seeds/01_users.js
@@ -3,9 +3,9 @@ exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('users').del()
     .then(function () {
-      // Inserts seed entries
       return knex('users').insert([
-        { id: 1, name: 'Frederick', username: 'Jimmy', password: 'password', user_type: 'parent', bank_account: 'YHGRB+zRxznmdsOV7QpZE5Ba25ut5nliF486mFhNgk=' }
+        { id: 1, name: 'Frederick', username: 'Jimmy', password: 'password', user_type: 'parent', bank_account: 'YHGRB+zRxznmdsOV7QpZE5Ba25ut5nliF486mFhNgk=' },
+        { id: 2, name: 'Nemo', username: 'Fish', password: 'password', user_type: 'child', bank_account: 'XFhWXJQOVdudjhONmdsOV7QpZE5Ba25ut5pa0N75jjoLJh=' }
       ])
     })
     .then(() => {
